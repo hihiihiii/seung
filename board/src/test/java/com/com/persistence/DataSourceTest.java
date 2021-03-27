860a0b7c625b13f73e52c83fb80d3392c1b327f3
ahrthfla <ahrthfla@naver.com>
2021-03-27 23:37:58 +0900

    0327 mybatis 연동시작

--- board/src/test/java/com/com/persistence/DataSourceTest.java
+++ board/src/test/java/com/com/persistence/DataSourceTest.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,20 @@ public class DataSourceTest {
 	
 	@Autowired
 	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
 
 	@Test
 	public void testConnection() {
-		try (Connection con = dataSource.getConnection();){
+		
+		//mybatis, database 연동 테스트
+		try (
+				Connection con = dataSource.getConnection();
+				SqlSession session = sqlSessionFactory.openSession();
+				){
 			System.out.println("con = "+con);
+			System.out.println("session = "+session);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
